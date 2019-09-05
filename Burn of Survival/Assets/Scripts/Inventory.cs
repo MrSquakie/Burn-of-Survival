8adc6e417aa89de43df2ed559d4fc1b586a33116
MrSquakie <45904658+MrSquakie@users.noreply.github.com>
2019-09-05 08:26:27 -0700

    Working on inventory system

---
+++ Burn of Survival/Assets/Scripts/Inventory.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject inventory;
+    public GameObject slotHolder;
+    private bool inventoryEnabled = false;
+
+    public int slots;
+    public Transform[] slot;
+
+    private GameObject itemPickedUp;
+
+    public void Start()
+    {
+        slots = slotHolder.transform.childCount;
+        print(slots);
+        slot = new Transform[slots];
+
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            print("yo");
+            if (inventoryEnabled)
+            {
+                inventory.SetActive(false);
+                inventoryEnabled = false;
+            }
+            else
+            {
+                inventoryEnabled = true;
+                inventory.SetActive(true);
+            }
+        }
+    }
+    public void toggleInventory()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Item>())
+        {
+            itemPickedUp = other.gameObject;
+            AddItem(itemPickedUp);
+        }
+    }
+
+    public void AddItem(GameObject item)
+    {
+        for (int i = 0; i < slots; i++)
+        {
+            if (slot[i].GetComponent<slot>.empty)
+            {
+
+            }
+        }
+    }
+    
+}
+    
