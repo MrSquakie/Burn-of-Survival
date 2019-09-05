a980f2a19d607983038eef71e253469c0127eb0d
Allen <49079375+ajfoust@users.noreply.github.com>
2019-09-05 15:21:31 -0700

    Added pond

---
+++ Burn of Survival/Assets/Scripts/Pond.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pond : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("click");
+            if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward))) {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward), Color.yellow);
+            }
+        }
+    }
+}
