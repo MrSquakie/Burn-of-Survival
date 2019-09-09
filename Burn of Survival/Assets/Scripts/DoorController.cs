e1f8383351d4f215037eadb91f2cdb583ebe2db3
Allen <49079375+ajfoust@users.noreply.github.com>
2019-09-08 19:39:48 -0700

    b4 desktop

---
+++ Burn of Survival/Assets/Scripts/DoorController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public Animator doorController;
+    public bool isAnimating = false;
+
+    private bool toggle = false;
+
+    public void Start()
+    {
+        doorController = GetComponent<Animator>();
+    }
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C) && toggle)
+        {
+            openDoor();
+            print("open");
+            toggle = !toggle;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.C) && !toggle)
+        {
+            closeDoor();
+            toggle = !toggle;
+            print("close");
+
+        }
+    }
+
+    public void openDoor()
+    {
+        doorController.SetBool("open", true);
+    }
+
+    public void closeDoor()
+    {
+        doorController.SetBool("open", false);
+
+    }
+}
