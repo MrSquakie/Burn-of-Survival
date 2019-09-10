42f2872ab4b6ae5cd623df213fd9066631dcdd74
MrSquakie <45904658+MrSquakie@users.noreply.github.com>
2019-09-10 01:50:28 -0700

    Visuals prev

---
+++ Burn of Survival/Assets/Scripts/Audio/AudioScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioClip sound;
+
+    public AudioSource soundSource;
+
+    void Start()
+    {
+        soundSource.clip = sound;
+        soundSource.Play();
+    }
+}
