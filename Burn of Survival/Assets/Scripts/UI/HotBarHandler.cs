1f238b59d934ff4434caee66c6ffd07008bb7ca9
MrSquakie <45904658+MrSquakie@users.noreply.github.com>
2019-09-10 01:07:40 -0700

    Preview

---
+++ Burn of Survival/Assets/Scripts/UI/HotBarHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotBarHandler : MonoBehaviour
+{
+    public Transform[] slot;
+    public GameObject hotBarHolder;
+    public void Start()
+    {
+        int numOfSlots = hotBarHolder.transform.childCount;
+        slot = new Transform[numOfSlots];
+        for (int i = 0; i < numOfSlots; i++)
+        {
+            slot[i] = hotBarHolder.transform.GetChild(i);
+            print(slot[i].transform.name);
+        }
+
+    }
+}
